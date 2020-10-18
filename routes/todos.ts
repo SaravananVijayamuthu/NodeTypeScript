473ae307ebaed0f82f509169a47b19de6cfafddc
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-18 21:37:21 +0530

    created post route

--- routes/todos.ts
+++ routes/todos.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';   //import express from 'express';
+import { Todo } from '../models/todos';
 
-const todos = [];
+const todos: Todo[] = [];
+
 const router = Router();            //const router = express.Router();
 
+//get
 router.get('/', (req, res, next) => {
     res.status(200).json({todos:todos});
+});
+
+//post
+router.post('/todo', (req, res, next) => {
+    const newTodo: Todo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+    todos.push(newTodo);
 });
 
 export default router;
