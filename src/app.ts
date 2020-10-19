0402d95f82ae4292692513814ae84b9723b97da4
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-19 11:55:20 +0530

    PORT chnged

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import todosRoutes from './routes/todos';
 import bodyParser from 'body-parser';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(todosRoutes);
 
 
-app.listen(3000);
-console.log("Check");
+app.listen(PORT, () => {
+    console.log('Server running on port %d', PORT);
+});
